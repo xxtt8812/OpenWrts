a9e1f5d21fee9ccdbf9b55b22f240373d58a1056
lj8812 <74142076+lj8812@users.noreply.github.com>
2025-02-13 11:27:43 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,7 +5,7 @@
 sed -i '/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.conf.default && sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\.05$/s/^/#/' feeds.conf.default
 
 # 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.1.1/192.168.0.20/g' package/base-files/files/bin/config_generate 
 
 # luci-theme-infinityfreedom
 echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
