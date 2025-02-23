c56a638b188476301fe1fbad41b2f16298646b83
xxtt8812 <105150612+xxtt8812@users.noreply.github.com>
2025-02-23 09:51:16 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -15,3 +15,6 @@ git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git pac
 
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
+# webrestriction
+git clone --depth 1 https://github.com/JSZMonkey/luci-app-control-webrestriction.git package/luci-app-control-webrestriction
