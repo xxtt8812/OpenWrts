b3498ec7cfb77a2fe2c83afb22e9af41e24e64cf
xxtt8812 <105150612+xxtt8812@users.noreply.github.com>
2025-02-23 20:01:42 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -11,6 +11,9 @@ sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
 # luci-theme-infinityfreedom
 echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 
+# luci-theme-infinityfreedom
+echo 'src-git webrestriction https://github.com/JSZMonkey/luci-app-control-webrestriction.git ' >>feeds.conf.default
+
 
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon 
